1bca38befde3064b28c285d915976b8aa4cb6194
OlloClark <ollo42@hotmail.co.uk>
2022-04-20 16:34:06 -0700

    first 5 complete

--- python-containers.py
+++ python-containers.py
@@ -26,5 +26,38 @@ for food in foods:
     elif food == foods[-2]:
         print(f"Exercise 3: {food}")
 
+# Exercise 4
+# Create a dictionary named home_town containing the keys of city, state and population.
+# Print a string with this format: "I was born in city, state - population of population"
+
+home_town = {
+    "city": "London",
+    "state": "(the UK doesn't have states- RULE BRITANNIA!)",
+    "population": "8.982 million"
+}
+    
+print(f"Exercise 4: I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
+
+# Exercise 5
+# Iterate over the key: value pairs in home_town and print a string for each item, for example:
+# "city = Arcadia"
+# "state = California"
+# "population = 58000"
+
+for key, val in home_town.items():
+    print(f"Exercise 5: {key} = {val}")
+
+# Exercise 6
+# Create an empty list named cohort.
+
+# Using a for loop, add one dictionary to the cohort list for each student name. 
+# Each dictionary should have this shape:
+
+#  {
+#    'student': 'Tina',
+#    'fav_food' 'Cheeseburger'
+#  }
+# Iterate over cohort printing out each element.
+
 
 
